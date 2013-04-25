2e5ecda26e0f96da64d3871fb976678e8a2e414d
Aşkın Yollu <askin@askin.ws>
2013-04-26 00:21:22 +0300

    rest of my el files

---
+++ elisp/init-packages.el
@@ -0,0 +1,30 @@
+;;; init-packages.el - setup dependent elisp packages via el-get and elpa
+;;
+;; based on Steve Purcell's init-el-get.el
+
+
+;; start ELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; ;; Local sources - recipes not included with el-get
+;; (setq el-get-sources
+;;       '(
+;;         (:name hungry-delete :type elpa)
+;;         (:name eproject
+;;                :type git :url "http://github.com/jrockway/eproject.git"
+;;                :features (eproject eproject-extras))
+;;         (:name idle-highlight-mode :type elpa)
+;;         (:name highlight-parentheses :type elpa)
+;;         (:name dlacewell-minimap
+;;                :type git :url "https://github.com/dustinlacewell/emacs-minimap.git"
+;;                :features minimap)))
+
+(setq packages
+      '(
+        linum-off
+        ))
+
+(provide 'init-packages)
