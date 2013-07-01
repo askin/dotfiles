4beb9cf9b2678faf0eb28cd80dcd2c75757d7fa7
Aşkın Yollu <askin@askin.ws>
2013-07-01 17:56:54 +0300

    change windows size for win-office and add color option to grep

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -8,9 +8,12 @@ alias nocaps="setxkbmap -option ctrl:nocaps"
 alias caps="setxkbmap -option -option capslock:caps"
 alias m='more'
 alias g='grep'
-alias win-office='rdesktop -g 1200x800 -k tr 194.27.61.118'
+alias win-office='rdesktop -g 1200x600 -k tr 194.27.61.118'
 alias ssh-office='ssh -l askin -p 3389 95.183.152.25'
 alias google-dns='echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null'
+
+# Alias override
+alias grep='grep --color=always'
 
 alias .="cd .."
 alias ..="cd ../.."
