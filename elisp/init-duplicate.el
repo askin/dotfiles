0b4fb12c63606afa590cbe275cad18b9fe7514b5
Aşkın Yollu <askin@askin.ws>
2013-08-06 12:12:05 +0300

    yeni configurasyonlar

---
+++ elisp/init-duplicate.el
@@ -0,0 +1,22 @@
+(defun djcb-duplicate-line (&optional commentfirst)
+  "comment line at point; if COMMENTFIRST is non-nil, comment the original"
+  (interactive)
+  (beginning-of-line)
+  (push-mark)
+  (end-of-line)
+  (let ((str (buffer-substring (region-beginning) (region-end))))
+    (when commentfirst
+    (comment-region (region-beginning) (region-end)))
+    (insert-string
+      (concat (if (= 0 (forward-line 1)) "" "\n") str "\n"))
+    (forward-line -1)))
+
+;; duplicate a line and comment the first
+(global-set-key
+ (kbd "C-c v")
+ (lambda()
+   (interactive)
+   (djcb-duplicate-line t)))
+
+;; duplicate a line
+(global-set-key (kbd "C-c d") "\C-a\C- \C-n\M-w\C-y")
