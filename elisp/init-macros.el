402994e11da5b82c96dd8ca390220132305acdbb
Aşkın Yollu <askin@askin.ws>
2014-10-31 11:02:50 +0200

    own macros added

---
+++ elisp/init-macros.el
@@ -0,0 +1,4 @@
+(fset 'orjinalton
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 19 42 42 42 32 77 101 110 252 1 11 134217788 19 42 42 42 42 42 32 left right 67108896 134217788 23 5 backspace backspace backspace backspace backspace backspace 14 1 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 11 19 42 42 42 42 127 1 67108896 134217790 23 82 44 105 116 backspace backspace backspace 105 116 109 58 32 35 70 79 88 backspace backspace 73 88 77 69 24 19] 0 "%d")) arg)))
+
+(provide 'init-macros)
